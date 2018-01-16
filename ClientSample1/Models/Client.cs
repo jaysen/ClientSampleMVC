2eb477351bac7ff091fbb4157ddff3dc4f958f1f
Jaysen Naidoo <jaysen@gmail.com>
2018-01-17 01:23:16 +0200

    Client model class

---
+++ ClientSample1/Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientSample1.Models
+{
+    public class Client
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public string EmailAddress { get; set; }
+        public string ContactNumber { get; set; }
+        public DateTime DateTimeStamp { get; set; } = DateTime.Now;
+        public bool Cancelled { get; set; }
+    }
+}
